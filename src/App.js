32e3bf025ca0c79b6e487cdb5fae63472316a980
sarra193 <slimensarra0@gmail.com>
2021-02-02 22:24:25 +0100

    changing filter function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import NavBar from './Componets/Header/NavBar/NavBar'
 import Footer from './Componets/Footer/Footer'
 import { Data } from './data'
@@ -8,33 +8,33 @@ import bg from './img/IoAdd.jpg'
 
 function App() {
 
-    const [rating, setRating] = useState("")
-    const [search, setSearch] = useState('')
-    const [movies, setMovies] = useState(Data)
+  const [rating, setRating] = useState("")
+  const [search, setSearch] = useState('')
+  const [movies, setMovies] = useState(Data)
 
   const handleChange = (value) => {
-      setRating(value);
-      }
-  
+    setRating(value);
+  }
+
   /* Search by name function */
-     
-
-  
-      
-      
-
-/* adding object to data */
-    const AddNewMovie=(x)=>{setMovies([...movies, x])}
 
 
-  return(
+
+
+
+
+  /* adding object to data */
+  const AddNewMovie = (x) => { setMovies([...movies, x]) }
+
+
+  return (
     <div style={{ backgroundImage: `url(${bg})` }}>
-      <NavBar movies={movies} setSearch={setSearch}  handleChange={handleChange} rating={rating}/>
-      
-      <MovieList movies={movies.filter((movie) => movie.Title.toLowerCase().includes(search.toLowerCase()) &&
+      <NavBar movies={movies} setSearch={setSearch} handleChange={handleChange} rating={rating} />
+
+      <MovieList movies={movies.filter((movie) => movie.Title.toLowerCase().includes(search.toLowerCase().trim()) &&
         movie.imdbRating >= rating)} AddNewMovie={AddNewMovie} />
 
-    <Footer/>
+      <Footer />
     </div>
   )
 }
